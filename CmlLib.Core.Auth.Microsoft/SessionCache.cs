ea9dfc0682af57f5b14633f0c8ce088f3651aeb1
AlphaBs <ksi123456ab@naver.com>
2022-06-03 00:39:00 +0900

    fix: session cache file property name

--- CmlLib.Core.Auth.Microsoft/SessionCache.cs
+++ CmlLib.Core.Auth.Microsoft/SessionCache.cs
@@ -1,11 +1,21 @@
 ﻿using XboxAuthNet.OAuth;
+using XboxAuthNet.XboxLive;
+using System.Text.Json.Serialization;
 
 namespace CmlLib.Core.Auth.Microsoft
 {
     public class SessionCache
     {
+        [JsonPropertyName("microsoftOAuthSession")]
         public MicrosoftOAuthResponse? MicrosoftOAuthSession { get; set; }
-        public AuthenticationResponse? XboxSession { get; set; }
+
+        [JsonPropertyName("xboxAuthSession")]
+        public XboxAuthResponse? XboxAuthSession { get; set; }
+
+        [JsonPropertyName("xboxSession")] // to keep backwards compatibility, it keeps old name
+        public MojangXboxLoginResponse? MojangSession { get; set; }
+
+        [JsonPropertyName("gameSession")]
         public MSession? GameSession { get; set; }
     }
 }
