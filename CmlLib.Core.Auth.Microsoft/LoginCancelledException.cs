fdee69487846bbb44fc3014e7068396913943306
AlphaBs <ksi123456ab@naver.com>
2022-06-24 00:14:54 +0900

    add LoginCancelledException

---
+++ CmlLib.Core.Auth.Microsoft/LoginCancelledException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CmlLib.Core.Auth.Microsoft
+{
+    public class LoginCancelledException : Exception
+    {
+        public LoginCancelledException(string message) : base(message)
+        {
+            
+        }
+    }
+}
